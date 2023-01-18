76976d21e4bde90d5cadda170274006f12e49c45
siandaniel <siandaniel@ymail.com>
2023-01-18 11:45:23 +0000

    add error handler for invalid sort_by criteria

--- app/models.js
+++ app/models.js
@@ -23,6 +23,10 @@ const fetchArticles = (topic, sort_by='created_at') => {
         }
     }
 
+    if (!['article_id', 'title', 'topic', 'author', 'body', 'created_at', 'votes', 'article_img_url'].includes(sort_by.toLowerCase())) {
+        return Promise.reject({status: 400, msg: "Bad request - invalid sort_by criteria"})
+    }
+
     sqlFetchArticlesQuery+= ` GROUP BY articles.article_id
                             ORDER BY articles.${sort_by} DESC`
     
