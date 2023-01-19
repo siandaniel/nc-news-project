d183fde0437a10f60ec1b6b02473208cb973ed28
siandaniel <siandaniel@ymail.com>
2023-01-19 11:20:35 +0000

    add comment_count feature to fetchArticleById model

--- app/models.js
+++ app/models.js
@@ -21,8 +21,13 @@ const fetchArticles = () => {
 
 
 const fetchArticleById = (article_id) => {
-    let sqlFetchArticleByIdQuery = `SELECT * FROM articles
-                                    WHERE article_id = $1`
+
+    let sqlFetchArticleByIdQuery =  `SELECT articles.*, COUNT(comments.article_id) AS comment_count 
+                                    FROM articles
+                                    LEFT JOIN comments ON articles.article_id = comments.article_id
+                                    WHERE articles.article_id = $1
+                                    GROUP BY articles.article_id
+                                    `
 
     return db.query(sqlFetchArticleByIdQuery, [article_id]).then(({ rows, rowCount }) => {
         if (rowCount === 0) {
